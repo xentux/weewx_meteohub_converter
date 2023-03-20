261b104e076f60ec78398002d4e7a3db49dd4d09
jpouyaud-perso <55016969+jpouyaud-perso@users.noreply.github.com>
2023-03-20 19:56:15 +0100

    Update sensor.py
    Added some new function to get the missed rain0 value

--- weewx_meteohub/sensor.py
+++ weewx_meteohub/sensor.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import statistics
+import math
 
 
 class Sensor:
@@ -36,9 +37,9 @@ class Sensor:
     def get_th_values(self):
         """ Returns the values (temp/hum) of a th sensor """
         return (
-            float(self.data[2]) / 10,
-            int(self.data[3]),
-            float(self.data[4]) / 10,
+            float(self.data[2]) / 10,   # tempOut
+            int(self.data[3]),          # hum
+            float(self.data[4]) / 10,   # Dew
         )
 
     def get_thb_values(self):
@@ -49,20 +50,23 @@ class Sensor:
             float(self.data[2]) / 10,  # temp
             int(self.data[3]),  # hum
             float(self.data[4]) / 10,  # dew
-            float(self.data[5]) / 10,  # baro
+            float(self.data[5]) / 10,  # baro QNH
             int(self.data[6]),  # forecast
             float(self.data[7]) / 10,  # pressure at sealevel
         )
 
     def get_th_mean_values(self, value_list):
         """ Returns the mean values of a given th-value list """
+        """ Return also Tmin and Tmax """
         if not len(value_list):
-            return None, None, None
+            return None, None, None, None, None
         t, h, d = map(list, zip(*value_list))
         return (
             round(statistics.mean(t), 1),
             int(round(statistics.mean(h), 0)),
             round(statistics.mean(d), 1),
+            round(min(t), 1),
+            round(max(t), 1),
         )
 
     def get_thb_mean_values(self, value_list):
@@ -101,31 +105,35 @@ class Sensor:
             round(statistics.mean(chill), 1),
         )
 
-    def get_sol_value(self):
-        """ Returns the value of a sol sensor """
-        return float(self.data[2])
-
-    def get_sol_mean_value(self, value_list):
-        """ Returns the mean value of a given sol-value list """
-        if len(value_list):
-            return int(round(statistics.mean(value_list), 0))
-        else:
-            return None
-
-    def get_uv_value(self):
-        """ Returns the value of a uv sensor """
-        return float(int(self.data[2]) / 10)
-
-    def get_uv_mean_value(self, value_list):
-        """ Returns the mean value of a given uv-value list """
-        if len(value_list):
-            return int(round(statistics.mean(value_list), 0))
-        else:
-            return None
 
     def get_rain_values(self):
         """ Returns the rain sensor values (rate/total) """
         return float(self.data[2]) / 10, float(self.data[4]) / 10
+
+    def get_rain_cumul(self, value_list):
+        """ Returns the last values of a rain total sensor """
+        if not len(value_list):
+            return None
+
+        rate, total = map(list, zip(*value_list))
+        rain_cumul = round(total[-1], 1)
+
+        return rain_cumul
+
+    def get_rain_mean_values_consolidate(self, value_list, last_cumul):
+        """ Returns the mean values of a rain sensor """
+        if not len(value_list):
+            return None, None
+        #if math.isnan(last_cumul):
+        #    return None, None
+        rate, total = map(list, zip(*value_list))
+        rain = round(((total[-1] - total[0])+(total[0]-last_cumul)), 1)
+
+        # Rain can't be negative and in january many rain sensors are
+        # resetted to 0 which leads to negative values
+        if rain < 0:
+            rain = 0.0
+        return round(statistics.mean(rate)), rain
 
     def get_rain_mean_values(self, value_list):
         """ Returns the mean values of a rain sensor """
